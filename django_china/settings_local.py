4a9cebb70ae675c5bde06924014400c2160b9606
wwq0327 <wwq0327@gmail.com>
2012-02-10 15:35:15 +0800

    论留言

--- django_china/settings_local.py
+++ django_china/settings_local.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.comments',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
